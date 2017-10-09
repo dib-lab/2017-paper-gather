145acd349120447d352c329cd64c6a6760bb6e2c
Luiz Irber <irberlui@msu.edu>
2017-10-09 15:08:38 -0400

    Update with rules for downloading kraken libraries and creating db

--- pipeline/hpcc/jobscript.sh
+++ pipeline/hpcc/jobscript.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+module use /opt/software/ged-software/modulefiles/
 module load anaconda
-source activate snakemake
+module unload python
+module swap GNU GNU/6.2
+source activate 2017-paper-gather
 
 {exec_job}
--- pipeline/hpcc/submit.sh
+++ pipeline/hpcc/submit.sh
@@ -1,16 +1,17 @@
 logdir=hpcc/log
 mkdir -p $logdir
 
-QSUB="qsub -l ncpus={threads} "
+QSUB="qsub -l nodes=1:ppn={threads} "
 QSUB="$QSUB -l walltime={cluster.time} -l mem={cluster.mem}"
-QSUB="$QSUB -o $logdir -e $logdir"
+QSUB="$QSUB -o $logdir -e $logdir -A ged"
 
 snakemake                               \
-    -j 3000                             \
+    -j 100                              \
     --cluster-config hpcc/cluster.yaml  \
     --js hpcc/jobscript.sh              \
     --rerun-incomplete                  \
     --keep-going                        \
     --latency-wait 10                   \
+    --max-jobs-per-second 1             \
     --use-conda                         \
     --cluster "$QSUB" $@
